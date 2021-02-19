e2a63d222f3cd738e4e0619c5ae13e28f3d31e99
Harris Usmani <harrisusmani@protonmail.com>
2021-02-19 12:55:40 +0500

    Added an error handling system and updated the comments.

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     
     const [blog, setBlog] = useState(null);
     const [isLoadingBlogs, setIsLoadingBlogs] = useState(true)
-    
+    const [error, setError] = useState(null)
 //Depreciated blog delete functionality below. The prop for this functionality has been deleted.
 
 // const handleDelete = (id) =>{
@@ -13,21 +13,32 @@ const Home = () => {
 //         setBlog(newBlogs)
 // }
 
+
+//Error handling and API/Database fetch.
 useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json()
+    fetch('http://localhost:8000/blogs')  //Enter your API/DB url here.
+        .then(response => {
+            if (!response.ok){            //If response is NOT ok then it throws an error message.
+                throw Error ('Could not fetch the data. Error!')        //Customize your error message here.
+            }
+            return response.json()
         })
         .then ((data) => {
             setBlog(data)
-            setIsLoadingBlogs(false)
+            setIsLoadingBlogs(false)       //Stops the loading message.
+            setError(null)                 //No error received obviously so it will be null. 
+        })
+        .catch((loadingError) => {         //.catch function for catching errors.
+            setError(loadingError.message) //Basically assigns the error message to to set error so it can be rendered onto the website.
+            setIsLoadingBlogs(false)       //Will not display the loading message since an error has occured. 
         })
 }, []) //<------ Dependency arrary 
 
 
     return (
         <div className='home'>
-            { isLoadingBlogs && <div>Loading blogs...</div>}
+            { error && <div>{error}</div>}      {/* Uses conditional rendering. If the the left hand side of the code is true aka if there is an error then an error message will be displayed on the screen.*/}
+            { isLoadingBlogs && <div>Loading blogs...</div>}      {/* Exactly the same thing as above, uses conditional rendering as well. */}
             {blog && <BlogList blog={blog} title="All Blogs!" />}
         </div>
     )
