b60a89bab2c0727fe6db30e6ee15504da0a80ff3
Harris U <harrisusmani@protonmail.com>
2021-02-21 20:03:18 +0500

    Finished the form to add blogs and some styling to go along with it.

--- src/create.js
+++ src/create.js
@@ -1,9 +1,27 @@
 const Create = () => {
     return (
-        <div className='create'>
+        <div className='createBlog'>
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog title: </label>
+                <input type='text'
+                required
+                />
+                <h2>Add a New Blog</h2>
+                <label>Blog content: </label>
+                <textarea>
+                    required
+                </textarea>
+                <label>Blog author:</label>
+                <select>
+                    <option value="Steve Jobs">Steve Jobs</option>
+                    <option value="Jeff Bezos">Jeff Bezos</option>
+                    <option value="Bill Gates">Bill Gates</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
     );
 }
- 
+
 export default Create
