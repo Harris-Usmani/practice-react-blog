1da066128adb29db64e61f2b27f92621cab820a5
Harris Usmani <harrisusmani@protonmail.com>
2021-02-24 12:10:51 +0500

    working on being less dependent on usefetch

--- src/useFetch.js
+++ src/useFetch.js
@@ -2,7 +2,7 @@
 
 //Imports
 import {useState, useEffect} from 'react';
-
+import response from './blogDatabase.json'
 //Error handling and API/Database fetch.
 const useFetch = (url) => {
 
@@ -11,22 +11,44 @@ const useFetch = (url) => {
     const [error, setError] = useState(null)
 
     useEffect(() => {
-        fetch(url)  //Enter your API/DB url here.
-            .then(response => {
-                if (!response.ok){            //If response is NOT ok then it throws an error message.
-                    throw Error('Could not fetch the data. Error!')        //Customize your error message here.
+        // fetch(url)  //Enter your API/DB url here.
+        //     .then(response => {
+        //         if (!response.ok){            //If response is NOT ok then it throws an error message.
+        //             throw Error('Could not fetch the data. Error!')        //Customize your error message here.
+        //         }
+        //     return response.json()
+        // })
+        // .then ((data) => {
+        //     setData(data)
+        //     setIsLoadingBlogs(false)       //Stops the loading message.
+        //     setError(null)                 //No error received obviously so it will be null. 
+        // })
+        // .catch((loadingError) => {         //.catch function for catching errors.
+        //     setError(loadingError.message) //Basically assigns the error message to to set error so it can be rendered onto the website.
+        //     setIsLoadingBlogs(false)       //Will not display the loading message since an error has occured. 
+        // })
+
+
+        setIsLoadingBlogs(true)
+        setTimeout(() => {
+            
+            setIsLoadingBlogs(false)
+
+            let request = url.split("/")
+            request = request.slice(3)
+            let newResponse = undefined
+            request.forEach(element => {
+                if(newResponse){
+                    newResponse = newResponse[element]
+                    return
                 }
-            return response.json()
-        })
-        .then ((data) => {
-            setData(data)
-            setIsLoadingBlogs(false)       //Stops the loading message.
-            setError(null)                 //No error received obviously so it will be null. 
-        })
-        .catch((loadingError) => {         //.catch function for catching errors.
-            setError(loadingError.message) //Basically assigns the error message to to set error so it can be rendered onto the website.
-            setIsLoadingBlogs(false)       //Will not display the loading message since an error has occured. 
-        })
+                newResponse = response[element]
+            });
+            console.log(newResponse)
+            setData(newResponse)
+        }, 2000);
+
+
     }, [url]) //<------ Dependency arrary 
 
     return {data, isLoadingBlogs, error}
